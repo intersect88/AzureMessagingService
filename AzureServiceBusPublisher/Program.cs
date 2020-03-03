917230edc50b299b64a88620f07f46466f9edbf1
intersect88 <intersect@anche.no>
2020-03-03 14:36:49 +0100

    Add solution with ASB Publisher and ASB Subscriber

---
+++ AzureServiceBusPublisher/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureServiceBusPublisher
+{
+    class Program
+    {
+        const string AzureServiceBusConnectionString = "AzureServiceBusConnectionString";
+        const string AzureServiceBusTopic = "demotopic";
+        static ITopicClient topicClient;
+
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Type the number of messages that you want to publish -> ");
+            int numberOfMessage = int.Parse(Console.ReadLine());
+            topicClient = new TopicClient(AzureServiceBusConnectionString, AzureServiceBusTopic);
+
+            for (int i = 0; i < numberOfMessage; i++)
+            {
+                string messageBody = $"Demo Message {i}";
+                var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+                await topicClient.SendAsync(message);
+            }
+            await topicClient.CloseAsync();
+        }
+    }
+}
---
+++ AzureServiceBusSubscriber/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AzureServiceBusSubscriber
+{
+    class Program
+    {
+        const string AzureServiceBusConnectionString = "AzureServiceBusConnectionString";
+        const string AzureServiceBusTopic = "demotopic";
+        const string AzureServiceBusSubscription = "demoTopicSubscription";
+        static ISubscriptionClient subscriptionClient;
+
+        static async Task Main(string[] args)
+        {
+            subscriptionClient = new SubscriptionClient(AzureServiceBusConnectionString, AzureServiceBusTopic, AzureServiceBusSubscription);
+
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+            {
+                AutoComplete = false,
+                MaxConcurrentCalls = 1,
+                
+            };
+
+            subscriptionClient.RegisterMessageHandler(
+                ProcessMessages, messageHandlerOptions
+            );
+
+            Console.ReadKey();
+
+            await subscriptionClient.CloseAsync();
+
+        }
+
+        static async Task ProcessMessages(Message message, CancellationToken token)
+        {
+            string messageReceived = Encoding.UTF8.GetString(message.Body);
+            Console.WriteLine("Received -> " + messageReceived);
+            await subscriptionClient.CompleteAsync(message.SystemProperties.LockToken);
+        }
+
+        static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            Console.WriteLine($"Exception: {exceptionReceivedEventArgs.Exception}");
+            return Task.CompletedTask;
+        }
+    }
+}

3695a49a950391acd4b185a8c767638d3ea4efbe
intersect88 <intersect@anche.no>
2020-03-03 15:19:43 +0100

    Add solution for Event Hubs

---
+++ EventHubsProducer/Program.cs
@@ -0,0 +1,31 @@
+﻿using Azure.Messaging.EventHubs.Producer;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHubsProducer
+{
+    class Program
+    {
+        const string EventHubsConnectionString = "EventHubsConnectionString";
+        const string EventHub = "eventhubdemo";
+
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Type the number of events that you want to publish -> ");
+            int numberOfEvent = int.Parse(Console.ReadLine());
+
+
+            await using var eventHubProducerClient = new EventHubProducerClient(EventHubsConnectionString, EventHub);
+            using EventDataBatch eventDataBatch = await eventHubProducerClient.CreateBatchAsync();
+
+            for (int i = 0; i < numberOfEvent; i++)
+            {
+                string eventBody = $"Demo Message {i}";
+                eventDataBatch.TryAdd(new Azure.Messaging.EventHubs.EventData(Encoding.UTF8.GetBytes(eventBody)));
+            }
+
+            await eventHubProducerClient.SendAsync(eventDataBatch);
+        }
+    }
+}
---
+++ EventHubsReceiver/Program.cs
@@ -0,0 +1,47 @@
+﻿using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Consumer;
+using Azure.Messaging.EventHubs.Processor;
+using Azure.Storage.Blobs;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHubsReceiver
+{
+    class Program
+    {
+        const string EventHubsConnectionString = "EventHubsConnectionString";
+        const string EventHub = "eventhubdemo";
+        const string AzureBlobStorageConnectionString = "AzureBlobStorageConnectionString";
+        const string AzureBlobStorageContainer = "eventhubcontainer";
+        public static async Task Main(string[] args)
+        {
+            string eventHubConsumerClient = EventHubConsumerClient.DefaultConsumerGroupName;
+
+            BlobContainerClient blobContainerClient = new BlobContainerClient(AzureBlobStorageConnectionString, AzureBlobStorageContainer);
+
+            EventProcessorClient eventProcessorClient = new EventProcessorClient(blobContainerClient, eventHubConsumerClient, EventHubsConnectionString, EventHub);
+            eventProcessorClient.ProcessEventAsync += ProcessingEvent;
+            eventProcessorClient.ProcessErrorAsync += ProcessingError;
+
+            await eventProcessorClient.StartProcessingAsync();
+
+            await Task.Delay(TimeSpan.FromSeconds(10));
+
+            await eventProcessorClient.StopProcessingAsync();
+
+        }
+
+        private static Task ProcessingEvent (ProcessEventArgs processEventArgs) 
+        {
+            Console.WriteLine("Received -> " + Encoding.UTF8.GetString(processEventArgs.Data.Body.ToArray()));
+            return Task.CompletedTask;
+        }
+
+        private static Task ProcessingError(ProcessErrorEventArgs processErrorEventArgs)
+        {
+            Console.WriteLine(processErrorEventArgs.Exception.Message);
+            return Task.CompletedTask;
+        }
+    }
+}
